c74361c2ea62c500db9e8805ff01e3fa5e931aba
mattea777 <mattea.gjika@gmail.com>
2021-11-18 10:55:54 +0100

    Deleted unnecessary code

--- Algorithm-visualizer/AST/ast.hpp
+++ Algorithm-visualizer/AST/ast.hpp
@@ -5,29 +5,6 @@
 
 #include <QAbstractItemModel>
 
-class AST : public QAbstractItemModel
-{
-    Q_OBJECT
-
-public:
-    explicit AST(QObject *parent = nullptr);
-
-    // Header:
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
-
-    // Basic functionality:
-    QModelIndex index(int row, int column,
-                      const QModelIndex &parent = QModelIndex()) const override;
-    QModelIndex parent(const QModelIndex &index) const override;
-
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
-
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-
-private:
-};
-
 enum var_type {
     string = 0,
     integer = 1,
