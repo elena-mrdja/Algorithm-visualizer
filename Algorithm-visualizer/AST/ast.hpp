10e3773341abc21bb21ecca0dd6206dae0381122
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2021-11-18 10:58:08 +0100

    dasha + milena + evdokia changes

--- Algorithm-visualizer/AST/ast.hpp
+++ Algorithm-visualizer/AST/ast.hpp
@@ -4,31 +4,7 @@
 #include <string>
 #include <iostream>
 #include <QAbstractItemModel>
-
-/*
-class AST : public QAbstractItemModel
-{
-    Q_OBJECT
-
-public:
-    explicit AST(QObject *parent = nullptr);
-
-    // Header:
-    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
-
-    // Basic functionality:
-    QModelIndex index(int row, int column,
-                      const QModelIndex &parent = QModelIndex()) const override;
-    QModelIndex parent(const QModelIndex &index) const override;
-
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
-
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-
-private:
-};
-*/
+using namespace std;
 
 enum var_type {
     unknown_var_type = 0,
@@ -73,37 +49,45 @@ enum jump_type {
 class AST {
 protected : AST();
 protected : ~AST();
+virtual std::string get_type() = 0; // Statement or Expression   (this is an abstract method which will be defined in subclasses Statement
+//and Expression and inherited by every other subclass below)
+virtual std::string get_subtype() = 0; // Block, Declaration, UnOp, BinOp, ...
+//(this is an abstract method which will be defined in subclasses Block, Declaration, ...)
 private:
     std::map<char, char> variables;
 };
 
-class Statement : AST {
+class Statement : public AST {
 protected : Statement();
 protected : ~Statement();
+std::string get_type(){return "Statement";};
 };
 
 enum expression_type {
     unknown_expression_type = 0
 };
 
-class Expression : AST {
+class Expression : public AST {
 protected : Expression();
 protected : Expression(expression_type t) {
         type = t;
     };
     ~Expression();
+std::string get_type(){return "Expression";};
 private:
     expression_type type = unknown_expression_type;
 };
 
-class Block : Statement {
+class Block : public Statement {
 protected : Block();
 protected : ~Block();
+std::string get_subtype(){return "Block";};
     //list of statements
 };
 
 
-class Declaration : Statement {
+
+class Declaration : public Statement {
 public:
     Declaration();
     ~Declaration();
@@ -119,6 +103,7 @@ public:
     }
     char get_value(){return value;};
     void set_value(char v){value = v;};
+    std::string get_subtype(){return "Declaration";};
 
 
 private:
@@ -129,7 +114,7 @@ private:
     //name + value of the variable or Variable + sth
 };
 
-class Assignment : Statement {
+class Assignment : public Statement {
     public:
     Assignment();
     ~Assignment();
@@ -138,31 +123,33 @@ class Assignment : Statement {
     std::string get_name(){return name;};
     void set_name(std::string n){name= n;}
     //x = 5; x = y
+    std::string get_subtype(){return "Assignment";};
 private:
     std::string name;
     Expression* value;
 };
 
-class Return : Statement {
+class Return : public Statement {
     Return();
     ~Return();
     void set_exp(Expression* e){exp = e;};
     Expression* get_exp(){return exp;};
+    std::string get_subtype(){return "Return";};
 private:
     Expression* exp;
 };
 
-class Print : Statement {
+class Print : public Statement {
     Print();
     ~Print();
     void set_exp(Expression* e){exp = e;};
     Expression* get_exp(){return exp;};
-    //attribute: expression to be printed
+    std::string get_subtype(){return "Print";};
 private:
     Expression* exp;
 };
 
-class Jump : Statement {
+class Jump : public Statement {
     Jump();
     Jump(char value);
     ~Jump();
@@ -174,22 +161,32 @@ class Jump : Statement {
         case 2: return "continue";
         }
   };
+    std::string get_subtype(){return "Jump";};
 private:
     jump_type value = unknown_jump_type;
 };
 
-class IfRest : Statement {
+class Decision : public Statement {
+    Decision();
+    ~Decision();
+    void set_condition(Expression* c){condition = c;};
+    std::string get_subtype(){return "Decision";};
+private:
+    Expression* condition;
+};
+
+class IfRest : public Statement {
 public:
    IfRest();
    ~IfRest();
    Block* get_block_stmt(){return block_stmt;};
    void set_block_stmt(Block* stmt){block_stmt = stmt;};
-   //atrributes: a block, or another IfElse
+   std::string get_subtype(){return "IfRest";};
 private:
     Block* block_stmt;
 };
 
-class IfElse : Statement {
+class IfElse : public Decision {
     IfElse();
     ~IfElse();
     //attributes: condition(expression), IfRest
@@ -197,12 +194,13 @@ class IfElse : Statement {
     Expression* get_condition(){return condition;};
     void set_else_stmt(IfRest* stmt){else_stmt = stmt;};
     IfRest* get_else_stmt(){return else_stmt;};
+    std::string get_subtype(){return "IfElse";};
 private:
     Expression* condition;
     IfRest* else_stmt;
 };
 
-class While : Statement {
+class While : public Decision {
 public:
     While();
     ~While();
@@ -210,39 +208,41 @@ public:
     Expression* get_condition(){return condition;};
     Block* get_block_stmt(){return block_stmt;};
     void set_block_stmt(Block* stmt){block_stmt = stmt;};
-
+    std::string get_subtype(){return "While";};
     //attributes: condition, block
 private:
     Expression* condition;
     Block* block_stmt;
 };
 
-class Variable : Expression {
+class Variable : public Expression {
     Variable();
     ~Variable();
     void set_name(char n){name = n;};
     void set_type(var_type t){type = t;};
     std::string get_name(){return name;};
-    var_type get_type(){return type;};
+    var_type get_var_type(){return type;};
+    std::string get_subtype(){return "Variable";};
 
 private:
     std::string name;
     var_type type = unknown_var_type;
 };
 
-class UnOp : Expression {
+class UnOp : public Expression {
     UnOp();
     ~UnOp();
     void set_operation(un_op op){operation = op;};
     void set_expression(Expression* e){expression = e;};
     un_op get_operation(){return operation;};
     Expression* get_expression(){return expression;};
+    std::string get_subtype(){return "UnOp";};
 private:
     un_op operation = unknown_un_op;
     Expression* expression;
 };
 
-class BinOp : Expression {
+class BinOp : public Expression {
     BinOp();
     ~BinOp();
     void set_operation(bin_op op){operation = op;};
@@ -267,19 +267,21 @@ class BinOp : Expression {
     }
     Expression* get_left_expression(){return left_exp;};
     Expression* get_right_expression(){return right_exp;};
+    std::string get_subtype(){return "BinOp";};
 private:
     bin_op operation = unknown_bin_op;
     Expression* left_exp;
     Expression* right_exp;
 };
 
-class Boolean : Expression {
+class Boolean : public Expression {
     //not sure if this is necessary but putting
     //it down for now
     Boolean();
     ~Boolean();
     void set_value(bool v){value = v;};
     bool is_true(){return value;};
+    std::string get_subtype(){return "Bool";};
 private:
     bool value;
 };
