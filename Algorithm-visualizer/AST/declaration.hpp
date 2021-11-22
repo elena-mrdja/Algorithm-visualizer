3592f4395692332eb3ba64137a226c93cf219a3d
Evdokia Gneusheva <dusya.gn@gmail.com>
2021-11-22 16:10:04 +0100

    I added method which insert variable to a list of variables

--- Algorithm-visualizer/AST/declaration.hpp
+++ Algorithm-visualizer/AST/declaration.hpp
@@ -1,6 +1,7 @@
 #include "ast.hpp"
 #include "statement.hpp"
 #include "variable.hpp"
+#include "block.hpp"
 #ifndef DECLARATION_HPP
 #define DECLARATION_HPP
 
@@ -23,6 +24,7 @@ public:
     void set_value(char v){value = v;};
     Variable get_variable(){return variable;};
     std::string get_subtype(){return "Declaration";};
+  //void add_variable_to_list(){Block variables; variables.insert({variable, value});};
 private:
     Variable variable;
     char value;
