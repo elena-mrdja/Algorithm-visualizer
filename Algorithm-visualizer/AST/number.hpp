2d22ce8e00090b8a823be5a9db2d3ea2f1ceb59f
milenanedeljkovic <milena.nedeljkovic@polytechnique.edu>
2021-11-22 13:36:31 +0100

    number + comment changes

--- Algorithm-visualizer/AST/number.hpp
+++ Algorithm-visualizer/AST/number.hpp
@@ -1,4 +1,23 @@
 #ifndef NUMBER_HPP
 #define NUMBER_HPP
+#include "expression.hpp"
+#include "ast.hpp"
+
+//enum var_type {
+//    unknown_var_type = 0,
+//    string = 1,
+//    integer = 2,
+//    character = 3,
+//    floating_point = 4
+
+//};
+
+class Number : Expression {
+    Number();
+    ~Number();
+private:
+    var_type type;
+    std::string value;
+};
 
 #endif // NUMBER_HPP
