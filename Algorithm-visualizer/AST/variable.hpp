a083c5020b42ba945d4037288278a37afe12c0d4
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2021-12-05 19:47:02 +0100

    final(ish) version of Variable

--- Algorithm-visualizer/AST/variable.hpp
+++ Algorithm-visualizer/AST/variable.hpp
@@ -1,37 +1,28 @@
-#include "expression.hpp"
-#include "ast.hpp"
-#include "expression.hpp"
 #ifndef VARIABLE_HPP
 #define VARIABLE_HPP
+#include "expression.hpp"
 
 class Variable : public Expression {
-    Variable();
-    ~Variable();
-    void set_name(char n){name = n;};
-    void set_type(var_type t){type = t;};
-    std::string get_name(){return name;};
-    std::string get_var_type(){
-        switch(type) {
-        case 0: return "unknown variable type";
-        case 1: return "string";
-        case 2: return "integer";
-        case 3: return "char";
-        case 4: return "floating_point";
-        }
-    }
-    std::string get_subtype(){return "Variable";};
-    double get_num_value(){
-        assert (type ==2 or type == 4);
-        return std::stod(current_value);
+public : Variable();
+public : ~Variable();
+public : void set_name(std::string n){name = n;};
+public : void set_value(bool val){
+        if (val) value = 1;
+        else value = 0;
+        type = "bool";
     };
-    bool get_bool_value(){
-
+public : void set_value(double val){
+        value = val;
+        type = "double";
     };
-
+public : std::string get_name(){return name;};
+public : double get_value(){return value;};
+public : std::string get_exp_type(){return type;};
+public : string get_type(){return "Variable";};
 private:
     std::string name;
-    var_type type = unknown_var_type;
-    std::string current_value;
+    double value = 0;
+    std::string type;
 };
 
 #endif // VARIABLE_HPP
