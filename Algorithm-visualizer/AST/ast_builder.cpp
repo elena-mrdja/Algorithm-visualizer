8589dfa2e53dd93c639aa2faa0d66595aeb1c620
milenanedeljkovic <80540793+milenanedeljkovic@users.noreply.github.com>
2021-12-08 23:32:40 +0100

    interpreter functions

---
+++ Algorithm-visualizer/AST/ast_builder.cpp
@@ -0,0 +1,6 @@
+#include "ast_builder.hpp"
+
+//the function that finalizes the building of the AST:
+list<Statement> ast(list<string>);
+//input: output of team 1 that gives us all the elements we need easily accessible
+//output: the AST
