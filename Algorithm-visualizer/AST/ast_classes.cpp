ba626e6339686582ba21fbb7c81b6a11713383fd
milenanedeljkovic <milena.nedeljkovic@polytechnique.edu>
2021-12-11 22:08:22 +0100

    moved definitions of read_statement and draw_flowchart to ast_classes.cpp

--- Algorithm-visualizer/AST/ast_classes.cpp
+++ Algorithm-visualizer/AST/ast_classes.cpp
@@ -263,7 +263,46 @@ Block* While::get_block_stmt(){return block_stmt;};
 void While::set_block_stmt(Block* stmt){block_stmt = stmt;};
 subtypes While::get_subtype(){return while_loop;};
 
+flowchart read_statement(Statement* stmt, int i, list<CacheList*> variables){
+    //returns a flowchart corresponding to the given statement
+    //this function is supposed to be used within the walker i will keep the line of the statement being read
+    //(so, if stmt is in the 20th line, i = 20)
+    subtypes stmt_type = stmt->get_subtype();
+    flowchart chart;
+    if (stmt_type == declaration){
+        chart.shape = rectangle;
+        chart.text = "Declare " + stmt->get_variable()->get_name();
+        VarNode var = VarNode(stmt->get_variable()->get_name(), stmt->get_variable()->get_value());
+        return chart;
+    }
+    if(stmt_type == assignment){
+        chart.shape = rectangle;
+        chart.text = "Assign " + stmt->get_name();
+        return chart;
+    };
+    if(stmt_type == ifelse){
+        chart.shape = diamond;
+        chart.text = stmt->get_condition()->get_text();
+        chart.first_block = stmt->block_stmt->num_statements();
+        chart.second_block = stmt->else_stmt->num_stmt;
+        return chart;
+    };
+    if(stmt_type == while_loop){
+        chart.shape = diamond;
+        chart.text = stmt->get_condition()->get_text();
+        chart.first_block = stmt->block_stmt->num_statements();
+        return chart;
+    };
+    return chart;
+};
 
+void draw_flowchart(Block* block){
+    int n = block->num_statements();
+    //initialize CacheList of length n
+    for (int i = 0; i < n; i++){
+        //flowchart* chart = read_statement(block->statements[i], i);
+    };
+};
 
 
 
