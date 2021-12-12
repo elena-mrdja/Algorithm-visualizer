b5e8d9acd6737db82f2d5605854331dc0b10e1ad
Evdokia Gneusheva <dusya.gn@gmail.com>
2021-12-12 15:12:40 +0100

    moved create_ast_from_parse from ast_classes.hpp to ast_builder.cpp

--- Algorithm-visualizer/AST/ast_builder.cpp
+++ Algorithm-visualizer/AST/ast_builder.cpp
@@ -4,3 +4,82 @@
 list<Statement> ast(list<string>);
 //input: output of team 1 that gives us all the elements we need easily accessible
 //output: the AST
+
+string create_ast_from_parse(string pre_ast) {
+    //UnOp
+    if (pre_ast[1] == '!'){
+        Negation ast;
+        ast.expression* = pre_ast[0];
+    }
+
+    //BinOp
+    if (pre_ast[1] == '+'){
+        Addition ast;
+        ast.left_exp* = pre_ast[0];
+        ast.right_exp* = pre_ast[2];
+    }
+    if (pre_ast[1] == '-'){
+        Subtraction ast;
+        ast.left_exp* = pre_ast[0];
+        ast.right_exp* = pre_ast[2];
+    }
+    if (pre_ast[1] == '*'){
+        Multiplication ast;
+        ast.left_exp* = pre_ast[0];
+        ast.right_exp* = pre_ast[2];
+    }
+    if (pre_ast[1] == '/'){
+        Division ast;
+        ast.left_exp* = pre_ast[0];
+        ast.right_exp* = pre_ast[2];
+    }
+    if (pre_ast[1] == '<'){
+        Mthan ast;
+        ast.left_exp* = pre_ast[0];
+        ast.right_exp* = pre_ast[2];
+    }
+    if (pre_ast[1] == '>'){
+        Lthan ast;
+        ast.left_exp* = pre_ast[0];
+        ast.right_exp* = pre_ast[2];
+    }
+    if (pre_ast[1] == '<='){
+        Meq ast;
+        ast.left_exp* = pre_ast[0];
+        ast.right_exp* = pre_ast[2];
+    }
+    if (pre_ast[1] == '>='){
+        Leq ast;
+        ast.left_exp* = pre_ast[0];
+        ast.right_exp* = pre_ast[2];
+    }
+    if (pre_ast[1] == '=='){
+        Eqeq ast;
+        ast.left_exp* = pre_ast[0];
+        ast.right_exp* = pre_ast[2];
+    }
+    if (pre_ast[1] == '&&'){
+        AndOp ast;
+        ast.left_exp* = pre_ast[0];
+        ast.right_exp* = pre_ast[2];
+    }
+    if (pre_ast[1] == '||'){
+        OrOp ast;
+        ast.left_exp* = pre_ast[0];
+        ast.right_exp* = pre_ast[2];
+    }
+
+    //Variable
+    if (pre_ast[0] == ('bool' || 'int' || 'double' || 'char' || 'list' || 'string') ){
+        Variable ast;
+        ast.name = pre_ast[1];
+        ast.value = pre_ast[2];
+        ast.type = pre_ast[0]
+    }
+
+    //Assignment
+    if (pre_ast[0] == 'x'){  //what shoul i put as a condition for assignement???
+        Assignment ast;
+        ast.var_name = pre_ast[0];
+    }
+};
