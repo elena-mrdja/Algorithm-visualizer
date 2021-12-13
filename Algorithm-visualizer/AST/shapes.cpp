4566a8cbf585a9ce28c525d6e01ab90e613eeb5f
goranovic37 <91254922+goranovic37@users.noreply.github.com>
2021-12-13 13:05:19 +0100

    Update shapes.cpp
    merging functions for drawing flowchart (to be merged with GUI)

--- Algorithm-visualizer/AST/shapes.cpp
+++ Algorithm-visualizer/AST/shapes.cpp
@@ -1,13 +1,50 @@
 #include "shapes.hpp"
 
-//the function that will be used by the graphics:
-list<list<string>> chart(list<Statement>);
-//input: an AST
-//output: list of lists of strings:
-/*
- [[Shape, String to copy-paste into the shape, (optional, for while and if): Number of statements in the block]]
-*/
-/*in case of if, the third element has: num1(num of statements in if) space num2(number of statements in else/ 0 if there is no else)*/ /*no nested loops allowed*/
+//function that creates struct for a flowchart for each statement
+flowchart read_statement(Statement* stmt, int i, list<map<Variable*, ValuesList*>*>* variables){
+    //returns a flowchart corresponding to the given statement
+    //this function is supposed to be used within the walker i will keep the line of the statement being read
+    //(so, if stmt is in the 20th line, i = 20)
+    subtypes stmt_type = stmt->get_subtype();
+    flowchart chart;
+    if (stmt_type == declaration){
+        chart.shape = rectangle;
+        chart.text = "Declare " + stmt->get_variable()->get_name();
+        Value var;
+        var.value = stmt->get_variable()->get_value();
+        //variables[i][stmt->get_variable()].add_value(var);
+        return chart;
+    }
+    if(stmt_type == assignment){
+        chart.shape = rectangle;
+        chart.text = "Assign " + stmt->get_name();
+        return chart;
+    };
+    if(stmt_type == ifelse){
+        chart.shape = diamond;
+        chart.text = stmt->get_condition()->get_text();
+        chart.first_block = stmt->block_stmt->num_statements();
+        chart.second_block = stmt->else_stmt->num_stmt;
+        return chart;
+    };
+    if(stmt_type == while_loop){
+        chart.shape = diamond;
+        chart.text = stmt->get_condition()->get_text();
+        chart.first_block = stmt->block_stmt->num_statements();
+        return chart;
+    };
+    return chart;
+};
 
-//during this fuction we also have to build the linked list for
-//variable tracking! it is IMPERATIVE NOW that we define WHERE we will keep this list!
+//function that need to be completed by GUI (for each statement, read it, create a struct and then use it for drawing)
+void draw_flowchart(Block* block){
+    int n = block->num_statements();
+    list<map<Variable*, ValuesList*>*>* variables = new list<map<Variable*, ValuesList*>*>[n];
+    //this list has to be initialized to have pointers to maps for each line
+    list<Statement*>::iterator i;
+    int j = 1;
+    for (i = block->get_statements()->begin(); i != block->get_statements()->end();i++){
+        flowchart chart = read_statement(*i, j, variables);
+        j++;
+    };
+};
